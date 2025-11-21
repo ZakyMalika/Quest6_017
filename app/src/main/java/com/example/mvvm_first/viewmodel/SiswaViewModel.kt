605d536465b74db0cad14aca79416d744b5f70ac
ZakyMalika <zakymalika0987@gmail.com>
2025-11-21 15:20:50 +0700

    membuat variabel yang menggunakan access modifier

--- app/src/main/java/com/example/mvvm_first/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/mvvm_first/viewmodel/SiswaViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.mvvm_first.viewmodel
 
-class SiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.mvvm_first.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+
+
+
+
+
 }
